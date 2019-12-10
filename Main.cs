8f013659ff619e2e8aaea359c919d52416009e2e
Rafał Babiarz <sauler1995@gmail.com>
2019-12-10 19:24:05 +0100

    First commit

---
+++ Main.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using Harmony;
+using UnityEngine;
+using CMS.Mods;
+
+namespace NoMoreAsking {
+	public class Main : Mod {
+		private HarmonyInstance _harmonyInstance;
+		
+		public override void Activate() {
+			_harmonyInstance = HarmonyInstance.Create("com.sauler.cms.nomoreasking");
+			_harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		public override void Deactivate() {
+			_harmonyInstance.UnpatchAll();
+		}
+
+		public override ModInfo GetInfo() {
+			return new ModInfo {
+				Name = "No more asking",
+				Description = "Now you can skip ask windows if you hold Shift before action",
+				Author = "Rafał Babiarz",
+				Version = "1.0.0"
+			};
+		}
+	}
+	
+	[HarmonyPatch(typeof(UIManager))]
+	[HarmonyPatch("ShowAskWindow")]
+	[HarmonyPatch(new[] { typeof(NewHash) })]
+	class NoMoreAskingPatch {
+		[HarmonyPrefix]
+		public static bool Prefix(NewHash hash) {
+			if (!Input.GetKey(KeyCode.LeftShift)) {
+				ModManager.Log("Button is not holden!");
+				return true;
+			}
+			
+			var type = hash.GetFromKey("WindowType") as string;
+			ModManager.Log("Button is holden! Window type: " + type);
+			if (type == "SellPerCondition")
+				return true;
+			
+			GameManager.Get().ButtonAccept(hash);
+			return false;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# NoMoreAsking
+
+This is a mod for Car Mechanic Simulator 2018. 
+It disables confirmation windows if you hold **Left Shift** when clicking.
+
+## Installation
+
+Download latest release from [here](https://github.com/Sauler/NoMoreAsking/releases/latest) and paste it in **Mods** folder in game directory.
